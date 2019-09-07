5e7af20ca0ae27803428e61677ebe6e7d67a451b
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-09-07 23:25:15 +0200

    the healthz endpoint should return metadata... in this case

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends BaseController
 
     public function healthz()
     {
-        return new JsonResponse(["statusCode" => 200, "service" => config('app.auth_base_url')], 200);
+        return new JsonResponse(["statusCode" => 200, "service" => config('app.metadata_base_url')], 200);
     }
 
     public function packages()
