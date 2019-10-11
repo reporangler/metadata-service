3cf3504e6ea749f2fadaed248ac0179e479be869
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-10-11 20:34:58 +0200

    use the same strategy for package-group routes as the other routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-use \RepoRangler\Entity\Repository;
+use RepoRangler\Entity\Repository;
+use RepoRangler\Entity\PackageGroup;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,8 +30,10 @@ $router->group(['middleware' => ['cors']], function() use ($router) {
         });
 
         $router->group(['prefix' => 'package-group'], function() use ($router) {
-            $router->get('/name/{name:[a-z\-\.]+}', 'PackageGroupController@findByName');
-            $router->get('/id/{id:[0-9]+}',         'PackageGroupController@findById');
+            $pattern = PackageGroup::PATTERN;
+
+            $router->get("/{name:$pattern}", 'PackageGroupController@findByName');
+            $router->get('/{id:[0-9]+}',         'PackageGroupController@findById');
             $router->get('/',                       'PackageGroupController@getList');
             $router->post('/',                      'PackageGroupController@create');
             $router->put('/',                       'PackageGroupController@update');
